cbf696954de8d348ceb26f64b2dc003c46b99a9a
Jesus Abel Osorio Vergara <abelosorio2001@gmail.com>
2024-04-17 01:01:07 -0300

    arreglos cart

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import indexRouter from './routers/index.router.js'
 import productsRouter from './routers/products.router.js'
+import cartsRouter from './routers/carts.router.js'
 import { __dirname } from './utils.js';
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
 app.use('/api', productsRouter);
+app.use('/api', cartsRouter);
 
 
 app.listen(8080, () =>{
