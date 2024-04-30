67c32ba29cb04502a05d2f7fcecf9303e7d2f770
Jesus Abel Osorio Vergara <abelosorio2001@gmail.com>
2024-04-29 21:32:55 -0300

    arreglos

--- src/app.js
+++ src/app.js
@@ -1,18 +1,32 @@
 import express from 'express'
-import indexRouter from './routers/index.router.js'
+// import indexRouter from './routers/index.router.js'
 import productsRouter from './routers/products.router.js'
 import cartsRouter from './routers/carts.router.js'
+import viewsRouter from './routers/views.router.js'
 import { __dirname } from './utils.js';
+import handlebars from 'express-handlebars'
+import { Server } from 'socket.io';
 
 const app = express();
+const httpServer = app.listen(8080, () =>{
+    console.log('Servidor Corriendo en el puerto 8080.');
+});
+
+const socketServer = new Server(httpServer)
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname+'/public'))
 
-app.use('/', indexRouter);
+app.engine('handlebars', handlebars.engine())
+
+app.set('views', __dirname+'/views')
+app.set('view engine', 'handlebars')
+
+app.use('/', viewsRouter);
 app.use('/api', productsRouter);
 app.use('/api', cartsRouter);
 
-
-app.listen(8080, () =>{
-    console.log('Servidor Corriendo en el puerto 8080.');
-});
+socketServer.on('connection', socket => {
+    console.log('Cliente conectado')
+})
--- src/routers/index.router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-
-const router = Router();
-
-router.get('/', async(req, res) =>{
-    res.status(200).send( {message: 'Bienvenidos al servidor en express js'})
-})
-
-export default router;
